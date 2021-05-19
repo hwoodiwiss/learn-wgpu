e46616f3cb2e7fd9d2486d192c89a82774b5a116
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-05-19 04:23:42 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,227 @@
+use futures::executor::block_on;
+use wgpu::include_spirv;
+use winit::{
+    dpi::PhysicalSize,
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+
+fn rgb_to_normalized(r: u8, g: u8, b: u8) -> wgpu::Color {
+    // Wish this could be const, but cant do fp arithmatic in const fn
+    wgpu::Color {
+        r: r as f64 / 255f64,
+        g: g as f64 / 255f64,
+        b: b as f64 / 255f64,
+        a: 1.0
+    }
+}
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+    bg_color: wgpu::Color,
+    render_pipeline: wgpu::RenderPipeline,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                power_preference: wgpu::PowerPreference::HighPerformance,
+            })
+            .await
+            .expect("Could not create adapter instance!");
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .expect("Could not get device from adapter!");
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: adapter
+                .get_swap_chain_preferred_format(&surface)
+                .expect("Could not get swapchain format!"),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        // Cornflour blue, because I 'member XNA
+        let bg_color = rgb_to_normalized(100, 149, 237);
+
+        let vs_module =
+            device.create_shader_module(&include_spirv!("shaders/basic.vert.spv"));
+        let fs_module =
+            device.create_shader_module(&include_spirv!("shaders/basic.frag.spv"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &vs_module,
+                entry_point: "main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &fs_module,
+                entry_point: "main",
+                targets: &[wgpu::ColorTargetState {
+                    format: sc_desc.format,
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent::REPLACE,
+                        alpha: wgpu::BlendComponent::REPLACE,
+                    }),
+                    write_mask: wgpu::ColorWrite::ALL,
+                }],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                clamp_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+        });
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+            bg_color,
+            render_pipeline,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = self.size.width;
+        self.sc_desc.height = self.size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        let frame = self.swap_chain.get_current_frame()?.output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Frame render pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.bg_color),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        Ok(())
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let evt_loop = EventLoop::new();
+
+    let window_size = PhysicalSize::new(1920, 1080);
+    let window = WindowBuilder::new()
+        .with_title("WGPU Rendering")
+        .with_inner_size(window_size)
+        .build(&evt_loop)
+        .expect("Failed to create window!");
+
+    let mut render_state = block_on(State::new(&window));
+    evt_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() && !render_state.input(event) => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::Resized(new_size) => render_state.resize(*new_size),
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                render_state.resize(**new_inner_size)
+            }
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            _ => {}
+        },
+        Event::RedrawRequested(_) => {
+            render_state.update();
+            match render_state.render() {
+                Ok(_) => {}
+                //On swapchain lost, recreate
+                Err(wgpu::SwapChainError::Lost) => render_state.resize(render_state.size),
+                // On OOM Exit
+                Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
+}
---
+++ src/shaders/basic.frag
@@ -0,0 +1,7 @@
+#version 450
+
+layout(location=0) out vec4 o_colour;
+
+void main() {
+	o_colour = vec4(0.603, 0.807, 0.921, 1.0);
+}
